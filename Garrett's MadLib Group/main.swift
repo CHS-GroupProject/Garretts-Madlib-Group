573da3725f6bb236def8c47af48635b8d029872b
Coder <coder@mainlab26.local>
2016-01-06 07:47:56 -0800

    Initial Commit

---
+++ Garrett's MadLib Group/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Garrett's MadLib Group
+//
+//  Created by Coder on 1/6/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
