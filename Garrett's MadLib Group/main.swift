ae357474ecdd9955092dde116b4bdbcb005db9b4
Guest User <coder@mainlab6.local>
2016-01-06 10:56:37 -0800

    hi

--- Garrett's MadLib Group/main.swift
+++ Garrett's MadLib Group/main.swift
@@ -12,6 +12,7 @@ print("Hello, World!")
 print("hello Welcome to Garrett's Group!")
 //Tai ._.
 //Hello - Jonah
+//Don't mind can be anything for name
 func input() -> String {
     let keyboard = NSFileHandle.fileHandleWithStandardInput()
     let inputData = keyboard.availableData
