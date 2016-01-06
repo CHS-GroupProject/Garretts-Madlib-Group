5206f441191c3b2ed01820c2608e789ba3609597
Coder <coder@mainlab26.local>
2016-01-06 10:58:59 -0800

    Assignments

---
+++ Garrett's MadLib Group/Garrett's Work.swift
@@ -0,0 +1,10 @@
+//
+//  Garrett's Work.swift
+//  Garrett's MadLib Group
+//
+//  Created by Coder on 1/6/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+import Foundation
+//Work on 3 Mad Libs and Menu
---
+++ Garrett's MadLib Group/Jonah's Work.swift
@@ -0,0 +1,11 @@
+//
+//  Jonah's Work.swift
+//  Garrett's MadLib Group
+//
+//  Created by Coder on 1/6/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+import Foundation
+// Jonah Please Work on Finding and typeing up 2 Mad Lib Excersizes
+ // And  Assisting in Creating The Menu (We Will all work as a Group on this
