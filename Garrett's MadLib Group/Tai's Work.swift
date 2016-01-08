73150554b11be702a928513227609fd3c6ba7737
Guest User <coder@mainlab6.local>
2016-01-08 11:04:17 -0800

    adfg

--- Garrett's MadLib Group/Tai's Work.swift
+++ Garrett's MadLib Group/Tai's Work.swift
@@ -37,7 +37,13 @@ func taiMadLib() {
     print("Favorite Movie")
     let favoriteMovie = input()
     
-    print("\(name) is walking to their favorite store, \(favoriteStore). Looks up at the sky, because apprently they like looking at sky stuff, and they saw the color \(color). They go to \(favoriteStore) and buy \(food). In this such store they meet a friend, named \(friendsName) and they talk about their favorite Movie which is \(favoriteMovie).")
+    print("Verb")
+    let Verb = input()
+    
+    print("TV Show")
+    let TVshow = input()
+    
+    print("\(name) is walking to their favorite store, \(favoriteStore). Looks up at the sky, because apprently they like looking at sky stuff, and they saw the color \(color). They go to \(favoriteStore) and buy \(food). In this such store they meet a friend, named \(friendsName) and they talk about their favorite Movie which is \(favoriteMovie). As I walked home slowly from the store there was something in the bushes, so I \(Verb) home. Got home I went straight to the couch put on the TV and played \(TVshow)")
     print("Play again?")
 }
 
