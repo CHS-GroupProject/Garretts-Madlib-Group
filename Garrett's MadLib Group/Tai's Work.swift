72a930eb1f738cb9c1af5b2508a410018af45f1c
Guest User <coder@mainlab6.local>
2016-01-12 10:49:37 -0800

    asdf

--- Garrett's MadLib Group/Tai's Work.swift
+++ Garrett's MadLib Group/Tai's Work.swift
@@ -10,7 +10,7 @@ import Foundation
 // Tai, Please Work on Finding and typeing up 2 Mad Lib Excersizes 
 // And  Assisting in Creating The Menu (We Will all work as a Group on this
 
-func input() -> String{
+func input2() -> String{
     let keyboard = NSFileHandle.fileHandleWithStandardInput()
     let inputData = keyboard.availableData
     let strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
@@ -69,30 +69,60 @@ func secondMadLib() {
     let person = input()
     
     print("Adjective")
+    let adjective1 = input()
     
     print("Article of Clothing")
+    let article = input()
     
     print("Noun")
+    let noun1 = input()
     
     print("A City")
+    let aCity = input()
     
     print("Plural Noun")
+    let pluralNoun1 = input()
     
     print("Adjective")
+    let adjective2 = input()
     
     print("Part Of The Body")
+    let partOfTheBody = input()
     
     print("Letter Of the Alphabet")
+    let letterOfTheAlphabet = input()
     
     print("Celebrity")
+    let celebrity = input()
     
     print("Plural Noun")
+    let pluralNoun2 = input()
     
     print("Adjective")
+    let adjective3 = input()
     
-    print("")
+    print("A Place")
+    let aPlace = input()
     
-    print("There are many _ ways to choose a/an _ to read. First, you could ask for recommendations from your friends and _. Just don't ask Aunt _ - she only reads _ books with _ - ripping goddesses on the cover. If your friends and family are no help, try checking out the _ Review in The _ TImes. If the _ featured there are too _ for your taste, try something a little more low-_, like _: The _ Magazine, or _ Magazine. You cold also choose a book the _-fashioned way. Head so your local library or _ and browse the shelves until something catches your _ Or, you could save yourself a whole lot of _ trouble and log on to www.bookish.com, the _ new website to _ for books! With all the time you'll save not having to search for _ you can read _ more books!")
+    print("Part of The Body")
+    let partOfTheBody1 = input()
+    
+    print("Adjective")
+    let adjective4 = input()
+    
+    print("Adjective")
+    let adjective5 = input()
+    
+    print("Verb")
+    let verb = input()
+    
+    print("Plural Noun")
+    let pluralNoun3 = input()
+    
+    print("Number")
+    let number = input()
+    
+    print("There are many \(adjective) ways to choose a/an \(noun) to read. First, you could ask for recommendations from your friends and \(pluralNoun). Just don't ask Aunt \(person) - she only reads \(adjective1) books with \(article)- ripping goddesses on the cover. If your friends and family are no help, try checking out the \(noun1) Review in The \(aCity) Times. If the \(pluralNoun1) featured there are too \(adjective2) for your taste, try something a little more low-\(partOfTheBody), like \(letterOfTheAlphabet): The \(celebrity) Magazine, or \(pluralNoun2) Magazine. You cold also choose a book the \(adjective3)-fashioned way. Head so your local library or \(aPlace) and browse the shelves until something catches your \(partOfTheBody1) Or, you could save yourself a whole lot of \(adjective4) trouble and log on to www.bookish.com, the \(adjective5) new website to \(verb) for books! With all the time you'll save not having to search for \(pluralNoun3) you can read \(number) more books!")
 }
 
 
